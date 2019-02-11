04b03d531cd39a6e2105f4ed005ff46576a61f03
DESKTOP-JK3G9K5\u-tor <golt7040@gmail.com>
2019-02-11 15:11:18 +0900

    php部分修正

--- index.php
+++ index.php
@@ -80,15 +80,7 @@
           <form>
   <fieldset class="form-group">
     <label for="city">Enter the name of a city.</label>
-    <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php 
-																										   
-																										   if (array_key_exists('city', $_GET)) {
-																										   
-																										   echo $_GET['city']; 
-																										   
-																										   }
-																										   
-																										   ?>">
+    <input type="text" class="form-control" name="city" id="city" placeholder="Eg. London, Tokyo" value = "<?php  if (array_key_exists('city', $_GET)) { echo $_GET['city'];  } ?>">
   </fieldset>
   
   <button type="submit" class="btn btn-primary">Submit</button>
